93b769ed79c6ce7ae6b778ef7e0e6e821a4229eb
edithjepletingketer <47848825+edithjepletingketer@users.noreply.github.com>
2019-04-17 13:17:10 -0700

    added my assignment

--- student.py
+++ student.py
@@ -1,15 +1,20 @@
 class Students:
-    def __init__(self,first_name,second_name,age):
+    def __init__ (self,first_name,second_name,age):
         self.first_name = first_name
         self.second_name = second_name
         self.age=age
+        
 
     def full_name(self):
-        name=self.first_name+second_name
+        name=self.first_name+self.second_name
         return name
 
     def year_of_birth(self):
         return 2019-self.age
 
     def greeting(self):
-        return("Hello {} you were born in 1996".format(full_name))
+        return "Hello {} you were born {}".format(self.first_name,2019-self.age)
+
+    def initials(self):
+        initial=self.first_name+self.second_name
+        return initial
