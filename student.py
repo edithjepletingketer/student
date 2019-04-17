9f324aee1ac76294568dd73c5fba79f3a4464be4
edithjepletingketer <47848825+edithjepletingketer@users.noreply.github.com>
2019-04-17 10:11:54 -0700

    added my classes codes

---
+++ student.py
@@ -0,0 +1,15 @@
+class Students:
+    def __init__(self,first_name,second_name,age):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.age=age
+
+    def full_name(self):
+        name=self.first_name+second_name
+        return name
+
+    def year_of_birth(self):
+        return 2019-self.age
+
+    def greeting(self):
+        return("Hello {} you were born in 1996".format(full_name))
